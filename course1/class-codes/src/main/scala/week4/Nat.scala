a80df2d90007abdb1e3115982105c74a86765d7b
honeyAndSw <honey.and.sw@gmail.com>
2016-06-26 20:52:01 +0900

    Lecture 4.1

---
+++ course1/class-codes/src/main/scala/week4/Nat.scala
@@ -0,0 +1,39 @@
+package week4
+
+/**
+  * Nat, peano number class.
+  * Positive number class with simple operations
+  */
+abstract class Nat {
+  def isZero: Boolean
+  def predecessor: Nat
+  def sucessor: Nat = new Succ(this)
+  def +(that: Nat): Nat
+  def -(that: Nat): Nat
+}
+
+object Zero extends Nat {
+  override def isZero = true
+
+  override def +(that: Nat): Nat = that
+
+  override def -(that: Nat): Nat = if (that.isZero) this else throw new Error("negative number.")
+
+  override def predecessor: Nat = throw new Error("Zero doesn't have a predecessor.")
+}
+
+/**
+  * Successor of n
+  * @example new Succ(4) conceptually represents '5'.
+  *          new Succ(4).predecessor = 4
+  *          new Succ(4).successor = new Succ(new Succ(4)) = 6
+  */
+class Succ(n: Nat) extends Nat {
+  override def isZero = false
+
+  override def +(that: Nat): Nat = new Succ(n + that)
+
+  override def -(that: Nat): Nat = if (that.isZero) this else n - that.predecessor
+
+  override def predecessor: Nat = n
+}
