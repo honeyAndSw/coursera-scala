38e89ec9a834627d1080e34cce46c37038517740
honeyAndSw <honey.and.sw@gmail.com>
2016-07-30 21:43:18 +0900

    Week2 HW. Fix neighbors test

--- course2/week2-streams/src/main/scala/streams/GameDef.scala
+++ course2/week2-streams/src/main/scala/streams/GameDef.scala
@@ -137,8 +137,7 @@ trait GameDef {
      * Returns the list of blocks that can be obtained by moving
      * the current block, together with the corresponding move.
      */
-    def neighbors: List[(Block, Move)] =
-      List((left, Left), (right, Right), (up, Up), (down, Down)).filter(_._1.isLegal)
+    def neighbors: List[(Block, Move)] = List((left, Left), (right, Right), (up, Up), (down, Down))
 
     /**
      * Returns the list of positions reachable from the current block
