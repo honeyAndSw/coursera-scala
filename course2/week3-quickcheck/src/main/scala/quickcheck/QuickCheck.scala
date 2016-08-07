bda4d871d6f1c9438a31d241bbfc388fb9205bf4
honeyAndSw <honey.and.sw@gmail.com>
2016-08-07 13:49:17 +0900

    Add initial week3 assignment project.

---
+++ course2/week3-quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -0,0 +1,21 @@
+package quickcheck
+
+import common._
+
+import org.scalacheck._
+import Arbitrary._
+import Gen._
+import Prop._
+
+abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
+
+  lazy val genHeap: Gen[H] = ???
+  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
+
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    // Insert min and find a minimum. They're should be equal.
+    findMin(insert(m, h)) == m
+  }
+
+}
