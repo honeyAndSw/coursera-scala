fecb6b15cd2e851a2e5a84616f75c9c44f9d47a3
honey.and.sw <honey.and.sw@gmail.com>
2017-05-29 10:08:07 +0900

    Course5 Observatory, 2nd milestone
    Course5 - 2nd milestone, WIP
    
    Course5 - 2nd milestone
    
    Course5 - 2nd milestone

--- course5/observatory/src/main/scala/observatory/Visualization.scala
+++ course5/observatory/src/main/scala/observatory/Visualization.scala
@@ -1,28 +1,109 @@
 package observatory
 
-import com.sksamuel.scrimage.{Image, Pixel}
+import com.sksamuel.scrimage.{Image, Pixel, Color => ScriImageColor}
 
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization {
 
+  private val MAX_KM_USE_GIVE_TEMP = 1
+
+  /* power parameter on the inverse distance weighting algorithm */
+  private val P: Double = 2
+  /* radius of Earth */
+  private val EARTH_RADIUS: Double = 6371
+
   /**
+    * Inverse distance weighting
+    * https://en.wikipedia.org/wiki/Inverse_distance_weighting
+    *
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
     * @param location Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-    ???
+    // from (location of known location, temperature of known location)
+    // to (distance of the two, temperature of known location) sorted by distance
+    val distTempTuples: Iterable[(Double, Double)] = temperatures.map { case (loc, temp) =>
+      val d = distance(loc, location)
+      (d, temp)
+    }.toSeq.sortBy(_._1)
+
+    val head = distTempTuples.head
+
+    if (head._1 <= MAX_KM_USE_GIVE_TEMP) {
+      head._2
+    }
+    else {
+      val dinom = distTempTuples.map{case (dist, _) => 1.0 / Math.pow(dist, P)}.reduce(_ + _)
+      val num = distTempTuples.map{case (dist, temp) => temp / Math.pow(dist, P)}.reduce(_ + _)
+
+      (num / dinom)
+    }
   }
 
   /**
+    * Great-circle distance
+    * https://en.wikipedia.org/wiki/Great-circle_distance
+    *
+    * http://www.movable-type.co.uk/scripts/latlong.html
+    * http://introcs.cs.princeton.edu/java/12types/GreatCircle.java.html
+    */
+  def distance(l1: Location, l2: Location): Double = {
+    def greatCircleDistance = {
+      val (lat1, lon1) = (l1.lat.toRadians, l1.lon.toRadians)
+      val (lat2, lon2) = (l2.lat.toRadians, l2.lon.toRadians)
+
+      val diffLat: Double = Math.abs(lat1 - lat2)
+      val diffLon: Double = Math.abs(lon1 - lon2)
+
+      val angle: Double = Math.acos(
+        Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(diffLon)
+      )
+
+      (angle * EARTH_RADIUS.toRadians).toDegrees
+    }
+
+    if (l1.equals(l2)) 0.0 else greatCircleDistance
+  }
+
+  /**
+    * Linear interpolation
+    * https://en.wikipedia.org/wiki/Linear_interpolation
+    *
     * @param points Pairs containing a value and its associated color
     * @param value The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
-    ???
+    // Sorted by temperatures
+    val tempSortedPoints = points.toSeq.sortBy(_._1)
+
+    if (tempSortedPoints(0)._1 >= value) tempSortedPoints(0)._2
+    else if (tempSortedPoints.last._1 <= value) tempSortedPoints.last._2
+    else {
+      val x0 = tempSortedPoints(tempSortedPoints.lastIndexWhere{case (temp, _) => temp <= value})
+      val x1 = tempSortedPoints(tempSortedPoints.indexWhere{case (temp, _) => temp >= value})
+
+      if (x0._1 == x1._1) x0._2
+      else linearInterpolateColor(x0, x1, value)
+    }
+  }
+
+  private def linearInterpolateColor(p1: (Double, Color), p2: (Double, Color), p: Double): Color =
+    Color(
+      linearInterpolate((p1._1, p1._2.red),   (p2._1, p2._2.red),   p),
+      linearInterpolate((p1._1, p1._2.green), (p2._1, p2._2.green), p),
+      linearInterpolate((p1._1, p1._2.blue),  (p2._1, p2._2.blue),  p)
+    )
+
+  private def linearInterpolate(p1: (Double, Int), p2: (Double, Int), p: Double): Int = {
+    val (x0, y0) = p1
+    val (x1, y1) = p2
+    val x = p
+
+    Math.round(((y0 * (x1 - x)) + (y1 * (x - x0))) / (x1 - x0)).toInt
   }
 
   /**
@@ -31,8 +112,22 @@ object Visualization {
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Double)], colors: Iterable[(Double, Color)]): Image = {
-    ???
-  }
+    val (width, height) = (360, 180)
+    val pixels: Array[Pixel] = new Array[Pixel](width * height)
 
+    for {
+      w <- 0 until width
+      h <- 0 until height
+    } yield {
+      val (lat, lon) = (-h + 90, w - 180)
+      val temp: Double = predictTemperature(temperatures, Location(lat, lon))
+      val color: Color = interpolateColor(colors, temp)
+
+      val pixel: Pixel = Pixel(ScriImageColor(color.red, color.green, color.blue))
+      pixels(h * width + w) = pixel
+    }
+
+    Image(width, height, pixels)
+  }
 }
 
--- course5/observatory/src/test/scala/observatory/VisualizationTest.scala
+++ course5/observatory/src/test/scala/observatory/VisualizationTest.scala
@@ -1,13 +1,84 @@
 package observatory
 
-
+import com.sksamuel.scrimage.Image
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.prop.Checkers
+import org.scalatest.Matchers._
 
 @RunWith(classOf[JUnitRunner])
 class VisualizationTest extends FunSuite with Checkers {
+  test("distance") {
+    val jfk = Location(40.6398010300, -73.7789001500)
+    val lhr = Location(51.4706001282, -0.4619410038)
+    val dist = Visualization.distance(jfk, lhr)
+    println(dist)
+  }
 
+  test("predictTemperature: some point closer") {
+    val location1 = Location(1,1)
+    val temp1 = 10d
+    val location2 = Location(-10,-10)
+    val temp2 = 50d
+    val list = List(
+      (location1, temp1),
+      (location2, temp2)
+    )
+    val result = Visualization.predictTemperature(list, Location(0, 0))
+    assert(temp1 - result < temp2 - result)
+  }
 
+  test("[#2 - Raw data display] (1) predicted temperature at location z should be closer to known temperature at location x than to known temperature at location y, if z is closer (in distance) to x than y, and vice versa") {
+    val temps = List(
+      (Location(45.0, -90.0), 10.0), (Location(-45.0, 0.0), 20.0)
+    )
+    // [Observed Error] 12.0 did not equal 10.0 +- 1.0E-4 Incorrect predicted temperature at Location(45.0,-90.0): 12.0. Expected: 10.0
+    val result = Visualization.predictTemperature(temps, Location(45.0, -90.0))
+    result should equal (10.0)
+  }
+
+  test("[#2 - Raw data display] (2) predicted temperature at location z should be closer to known temperature at location x than to known temperature at location y, if z is closer (in distance) to x than y, and vice versa") {
+    val temps = List(
+      (Location(45.0, -90.0), 10.0), (Location(-45.0, 0.0), 20.0)
+    )
+    val result = Visualization.predictTemperature(temps, Location(90.0, -180.0))
+    assert(Math.abs(10.0 - result) < Math.abs(20.0 - result))
+  }
+
+  test("[#2 - Raw data display] (1) color interpolation") {
+    Visualization.interpolateColor(
+      List((-1.0, Color(255,0,0)), (0.0, Color(0,0,255))),
+      -0.75
+    ) should equal (Color(191, 0, 64))
+  }
+
+  test("[#2 - Raw data display] (2) color interpolation") {
+    Visualization.interpolateColor(
+      List(
+        (-2.147483648E9,Color(255,0,0)), (1.0,Color(0,0,255))
+      ),
+      -1.61061273575E9
+    ) should equal (Color(191, 0, 64))
+  }
+
+  test("calculate pixel index") {
+    def idx(loc: Location): Int =
+      ((-loc.lat + 90) * 360 + (loc.lon + 180)).toInt
+
+    idx(Location(90, -180)) should equal (0)
+    idx(Location(90, 179)) should equal (359)
+    idx(Location(-89, -180)) should equal (179 * 360)
+  }
+
+  test("[#2 - Raw data display] visualize") {
+    val temps = List(
+      (Location(45.0,-90.0), 100.0), (Location(-45.0,0.0), 1.0)
+    )
+    val colors = List(
+      (100.0, Color(255,0,0)), (1.0, Color(0,0,255))
+    )
+
+    Visualization.visualize(temps, colors)
+  }
 }
